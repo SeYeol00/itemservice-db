3f7f5b9347e2ae32d58fde2044816781ccda221b
SeYeol_Park <79959576+SeYeol00@users.noreply.github.com>
2022-11-10 16:54:08 +0900

    11/10

--- src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -3,6 +3,7 @@ package hello.itemservice.repository.jpa;
 import hello.itemservice.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,10 +13,12 @@ public interface SpringDataJpaItemRepository extends JpaRepository<Item,Long> {
 
     List<Item> findByPriceLessThanEqual(Integer price);
 
-    // 쿼리 매서드
+    // 쿼리 매서드(아래 매서드와 같은 기능 수행)
     List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName,Integer price);
 
     // 쿼리 직접 실행, jpql 직접 작성,:는 파라미터 바인딩
     @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
-    List<Item> findItems(String itemName ,Integer price);
+    List<Item> findItems(@Param("itemName") String itemName , @Param("price")Integer price);
+
+
 }
